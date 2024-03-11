428633eb9067f2186b6ea56f2c23c84d0f18ff4b
Matheusgabriel1234 <magaje2005@gmail.com>
2024-03-11 11:40:13 -0300

    Projeto Criado

---
+++ src/main/java/com/Matheusgabriel1234/workshop/WorkshopSpringMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.Matheusgabriel1234.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Matheusgabriel1234/workshop/WorkshopSpringMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Matheusgabriel1234.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
