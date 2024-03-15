64fd5c7b42a06600810fdd818b178a11beb9f5ad
Matheusgabriel1234 <magaje2005@gmail.com>
2024-03-15 12:56:49 -0300

    Postagens dos usúarios adcionadas no Banco de dados

--- src/main/java/com/Matheusgabriel1234/workshop/config/Config.java
+++ src/main/java/com/Matheusgabriel1234/workshop/config/Config.java
@@ -1,30 +1,44 @@
 package com.Matheusgabriel1234.workshop.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.Matheusgabriel1234.workshop.domain.Post;
 import com.Matheusgabriel1234.workshop.domain.Users;
+import com.Matheusgabriel1234.workshop.repository.PostRepository;
 import com.Matheusgabriel1234.workshop.repository.UserRepository;
 
 @Configuration
 public class Config implements CommandLineRunner {
-
+	
+	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+  
+	
+	
 	@Autowired
-	UserRepository repo;
+	private UserRepository repo;
+	
+	@Autowired
+	private PostRepository postRepo;
 	
 	@Override
 	public void run(String... args) throws Exception {
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		// TODO Auto-generated method stub
+		postRepo.deleteAll();
 		repo.deleteAll();
 		Users eduarda = new Users(null,"Eduarda Cecato","eduarda@gmail.com");
 		Users luiza = new Users(null,"Luiza Grando","luiza@gmail.com");
 		Users everton = new Users(null,"Everton Cecato","everton@gmail.com");
-		
+		Post post1 = new Post(null,sdf.parse("21/03/2018"),"Partiu viagem","Vou viajar para SC abraços",luiza);
+		Post post2 = new Post(null,sdf.parse("21/03/2018"),"Partiu viagem","Vou viajar para o RJ abraços",eduarda);
 		repo.saveAll(Arrays.asList(eduarda,luiza,everton));
+		postRepo.saveAll(Arrays.asList(post1,post2));
 	}
 	
 
